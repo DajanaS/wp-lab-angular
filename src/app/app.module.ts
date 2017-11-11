ecbf9751a77804d27e1a7652f9726f159a164a90
DajanaS <dajana.sk@hotmail.com>
2017-11-11 23:35:29 +0100

    Little style to DetailsComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
 
 import {AppComponent} from './app.component';
 import {StudentDetailsComponent} from './student-details/student-details.component';
