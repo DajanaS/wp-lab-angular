09b0cd1fa07ebe9d6f6a18fa4e9ba0562d5f1b9e
DajanaS <dajana.sk@hotmail.com>
2017-11-11 19:18:24 +0100

    StudentList component is generated and list is default path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,18 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {StudentDetailsComponent} from './student-details/student-details.component';
 import {StudentManagementService} from './student-management.service';
+import { StudentListComponent } from './student-list/student-list.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    StudentDetailsComponent
+    StudentDetailsComponent,
+    StudentListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [StudentManagementService],
   bootstrap: [AppComponent]
