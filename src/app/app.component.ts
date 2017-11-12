18fabfcdfd75403f68c6696ae9918498968b8d9d
DajanaS <dajana.sk@hotmail.com>
2017-11-12 21:18:00 +0100

    Disable submit if form not valid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import {Component} from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Lab 1 - Student Management Service';
+  title = 'Lab - Student Management Service';
 }
